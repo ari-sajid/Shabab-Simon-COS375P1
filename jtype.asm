5c75441418df9817b4bb90261a6ea61bac5dd4d9
Ariyan Sajid <134431399+ari-sajid@users.noreply.github.com>
2024-10-05 14:55:56 -0400

    Delete jtype.asm

--- jtype.asm
+++
@@ -1,8 +0,0 @@
-addi $t0, $zero, 100   # Load 100 into $t0
-addi $t1, $zero, 50    # Load 50 into $t1
-jal  func              # Jump and link to `func`, save return address in $ra
-.word 0xfeedfeed       # Halt instruction (exit)
-
-func:
-    add  $t2, $t0, $t1  # Add $t0 and $t1, store result in $t2 (should be 150)
-    jr   $ra            # Return to address stored in $ra
