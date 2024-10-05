33417c7f25567ce996112c8272a4453cc79bf11f
Ariyan Sajid <134431399+ari-sajid@users.noreply.github.com>
2024-10-05 14:55:09 -0400

    Delete add.asm

--- add.asm
+++
@@ -1,6 +0,0 @@
-addi $t0, $zero, 25    # Load 25 into $t0
-addi $t1, $zero, 17    # Load 17 into $t1
-add  $t2, $t0, $t1     # Add $t0 and $t1, store result in $t2 (25 + 17 = 42)
-add  $t3, $t0, $zero   # Add $t0 and $zero, store result in $t3 (25)
-add  $t4, $t1, $zero   # Add $t1 and $zero, store result in $t4 (17)
-.word 0xfeedfeed       # Halt instruction (exit)
